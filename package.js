793e9d790611eff91e160d1974c68294808b00bd
Morten Henriksen <mh@gi2.dk>
2015-02-01 19:34:03 +0100

    Bump to version 1.3.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:date-tools',
   summary: 'Timezone aware date helpers.',
-  version: '1.3.2',
+  version: '1.3.3',
   git: 'https://github.com/DispatchMe/meteor-date-tools.git'
 });
 
